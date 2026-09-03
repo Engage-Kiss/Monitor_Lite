bc53bc19b1d42d17a6d1e93b8f0065d678424a5a
EngageKiss <tdt878yfd@outlook.com>
2026-09-04 00:46:20 +0800

    Update Monitor_Lite_Server.sh

--- Monitor_Lite/Monitor_Lite_Server.sh
+++ Monitor_Lite/Monitor_Lite_Server.sh
@@ -6,9 +6,9 @@ Monitor_URL="${2:-}"
 Monitor_Token="${3:-}"
 
 if [[-z "${1:-}" || -z "${2:-}" || -z "${3:-}"]]
-  echo "Usage: $0 <Monitor_Name> <Monitor_URL> <Monitor_Token>"
-  exit 1
+    echo "Usage: $0 <Monitor_Name> <Monitor_URL> <Monitor_Token>"
+    exit 1
 fi
 
 get_cpu_usage() {
-  read -r _ user nice system idle iowait irq softirq steal _ < /proc/stat
+    read -r _ user nice system idle iowait irq softirq steal _ < /proc/stat
